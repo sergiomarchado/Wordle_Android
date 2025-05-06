9fd504af6742006a5162e44644bd37f81dd36f4e
Sergio Marchado <36813062+sergiomarchado@users.noreply.github.com>
2025-05-06 20:22:01 +0200

    Commit Inicial con fragments renombrados

---
+++ app/src/main/java/com/example/ejercicioexamenwordle/GameFragment.kt
@@ -0,0 +1,44 @@
+package com.example.ejercicioexamenwordle
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.ejercicioexamenwordle.databinding.FragmentGameBinding
+
+/**
+ * A simple [Fragment] subclass as the second destination in the navigation.
+ */
+class GameFragment : Fragment() {
+
+    private var _binding: FragmentGameBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentGameBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonSecond.setOnClickListener {
+            findNavController().navigate(R.id.action_GameFragment_to_WelcomeFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
